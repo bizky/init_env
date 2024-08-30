05e18c09d412f68527c87096462d583e1375a4ad
root <root@beaming-lasers-3.localdomain>
2024-08-30 03:02:43 +0000

    add git-clone

--- init.sh
+++ init.sh
@@ -53,6 +53,55 @@ echo "
 $(declare -f docker-pull)
 " >> ~/.bashrc
 
+#####################################################################
+# Install Custom git-clone
+#####################################################################
+
+function git-clone() {
+
+  # 检查参数数量
+  if [ $# -ne 1 ]; then
+    echo "用法: git_clone_with_mirror <仓库地址>"
+    return 1
+  fi
+
+  # 提取用户名和仓库名
+  repo_url="$1"
+  username=$(echo "$repo_url" | sed -E 's#https://github.com/([^/]+)/([^/]+).git#\1#')
+  repo_name=$(echo "$repo_url" | sed -E 's#https://github.com/([^/]+)/([^/]+).git#\2#')
+
+  # 使用镜像地址克隆仓库
+  git clone "https://gh.hotmonitor.top/${username}/${repo_name}.git"
+
+  # 检查克隆是否成功
+  if [ $? -ne 0 ]; then
+    echo "克隆仓库失败"
+    return 1
+  fi
+
+  # 进入仓库目录
+  cd "$repo_name"
+
+  # 修改远程仓库地址为原始地址
+  git remote set-url origin "$repo_url"
+
+  # 检查修改是否成功
+  if [ $? -ne 0 ]; then
+    echo "修改远程仓库地址失败"
+    return 1
+  fi
+
+  echo "仓库克隆成功，并已将远程仓库地址修改为原始地址"
+  cd -
+
+}
+
+echo "
+# git-clone 函数
+$(declare -f git-clone)
+" >> ~/.bashrc
+
+
 
 
 source ~/.bashrc
